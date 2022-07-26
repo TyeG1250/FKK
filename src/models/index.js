207a80df4c0b0f881bcd34eb57d51f35672e2bd3
dannpl <dancardosobr@gmail.com>
2022-07-26 15:50:46 -0300

    feat: replace database var

--- src/models/index.js
+++ src/models/index.js
@@ -11,9 +11,9 @@ if (process.env.DISABLE_DB_LOGGING === 'true') {
 }
 
 let sequelize
-if (process.env.HEROKU_POSTGRESQL_COPPER_URL) {
+if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(
-    process.env[process.env.HEROKU_POSTGRESQL_COPPER_URL],
+    process.env[process.env.DATABASE_URL],
     config,
   )
 } else {
