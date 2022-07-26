c3b08d1f8338125a03799f28a278dea5f167233e
dannpl <dancardosobr@gmail.com>
2022-07-26 16:00:44 -0300

    feat: update vars

--- src/models/index.js
+++ src/models/index.js
@@ -11,9 +11,9 @@ if (process.env.DISABLE_DB_LOGGING === 'true') {
 }
 
 let sequelize
-if (process.env.DATABASE_URL) {
+if (process.env.HEROKU_POSTGRESQL_COPPER_URL) {
   sequelize = new Sequelize(
-    process.env[process.env.DATABASE_URL],
+    process.env[process.env.HEROKU_POSTGRESQL_COPPER_URL],
     config,
   )
 } else {
